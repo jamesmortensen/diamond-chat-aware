14d8331371cc892c359160f5b73d3b65de6aa0fe
James Mortensen <james.mortensen@a-cti.com>
2014-08-30 15:53:14 -0700

    Removed the diamonds in the textbox placeholder. Decreased opacity value to make watermark more subtle. Added CSS property to allow click-through to links behind the watermark.

--- src/js/main.js
+++ src/js/main.js
@@ -6,48 +6,35 @@ forge.logging.debug("Add diamond placeholders: placeholder=♦");
 
 
 /**
- * Create a string of chars of length iterations and return it.
- */
-function printerate(char, iterations) {
-	var text = '';
-    for(var i = 0; i < iterations; i++){
-        text += char;
-    }
-    return text;
-}
-
-
-/**
  * If the room is "locked", show the diamond watermark, and populate the textbox with diamonds.
  */
 var isLockedRoom = document.querySelector('.sprite-sec-private');
 if(isLockedRoom !== null) {
-	document.getElementById('input').setAttribute('placeholder', printerate('♦', 356));
 
-	/**
-	 * Inject watermark into page.
-	 */
-	forge.tools.getURL("images/diamond.png", function (myUrl) {
-	    var url = myUrl;
-	    var chatContainer = document.getElementById('chat');
-	    var diamondImg = document.createElement('img');
-	    diamondImg.src = url;
-	    diamondImg.className = 'diamondchat';
-	    chatContainer.appendChild(diamondImg);
-	});
+    /**
+     * Inject watermark into page.
+     */
+    forge.tools.getURL("images/diamond.png", function (myUrl) {
+        var url = myUrl;
+        var chatContainer = document.getElementById('chat');
+        var diamondImg = document.createElement('img');
+        diamondImg.src = url;
+        diamondImg.className = 'diamondchat';
+        chatContainer.appendChild(diamondImg);
+    });
 
-	/**
-	 * Inject CSS stylesheet onto page.
-	 */
-	forge.tools.getURL("css/diamond.css", function (myUrl) {
-	    var url = myUrl;
-	    var headContainer = document.head;
-	    var diamondCss = document.createElement('link');
-	    diamondCss.rel = 'stylesheet';
-	    diamondCss.href = url;
-	    headContainer.appendChild(diamondCss);
-	});
-	
+    /**
+     * Inject CSS stylesheet onto page.
+     */
+    forge.tools.getURL("css/diamond.css", function (myUrl) {
+        var url = myUrl;
+        var headContainer = document.head;
+        var diamondCss = document.createElement('link');
+        diamondCss.rel = 'stylesheet';
+        diamondCss.href = url;
+        headContainer.appendChild(diamondCss);
+    });
+    
 }
 
 
